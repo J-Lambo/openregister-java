0d77233cbed0f1744e32f3defa49ffebb13614a5
John Ollier <john.ollier@digital.cabinet-office.gov.uk>
2017-03-23 23:44:48 +0000

    Record(s) to include key but not item-hash.

--- src/main/java/uk/gov/register/core/Entry.java
+++ src/main/java/uk/gov/register/core/Entry.java
@@ -20,7 +20,7 @@ import java.time.Instant;
 import java.util.*;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonPropertyOrder({"index-entry-number", "entry-number", "entry-timestamp", "item-hash", "key"})
+@JsonPropertyOrder({"index-entry-number", "entry-number", "entry-timestamp", "key", "item-hash"})
 public class Entry implements CsvRepresentationView<Entry> {
     private final int entryNumber;
     private final List<HashValue> hashValues;
--- src/test/java/uk/gov/register/core/PostgresRegisterTest.java
+++ src/test/java/uk/gov/register/core/PostgresRegisterTest.java
@@ -8,12 +8,7 @@ import uk.gov.register.exceptions.NoSuchFieldException;
 import uk.gov.register.service.ItemValidator;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Optional;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.*;
 import static uk.gov.register.db.InMemoryStubs.inMemoryEntryLog;
 import static uk.gov.register.db.InMemoryStubs.inMemoryItemStore;
@@ -44,25 +39,6 @@ public class PostgresRegisterTest {
         when(registerFieldsConfiguration.containsField("name")).thenReturn(true);
         register.max100RecordsFacetedByKeyValue("name", "United Kingdom");
         verify(recordIndex, times(1)).findMax100RecordsByKeyValue("name", "United Kingdom");
-    }
-
-    @Test
-    public void shouldReturnRecordWithItems() {
-        Entry entry = mock(Entry.class);
-        Item item = mock(Item.class);
-        Record record = new Record(entry, Collections.singleton(item));
-        when(recordIndex.getRecord("k")).thenReturn(Optional.of(record));
-        Optional<Record> recordOptional = register.getRecord("k");
-        assertThat(recordOptional.get().getEntry(), is(entry));
-    }
-
-    @Test
-    public void shouldReturnEmptyWhenNoItems() {
-        Entry entry = mock(Entry.class);
-        Optional<Record> recordOpt = Optional.of(new Record(entry, Collections.EMPTY_SET));
-        when(recordIndex.getRecord("k")).thenReturn(recordOpt);
-        Optional<Record> recordOptional = register.getRecord("k");
-        assertFalse(recordOptional.isPresent());
     }
 
     private RegisterMetadata registerMetadata(String registerName) {
